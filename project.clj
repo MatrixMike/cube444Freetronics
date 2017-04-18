5a67152d6f8812caa70c6b9db8d74fd054bbdbf5
MikeH <matroxmike@icloud.com>
2017-04-18 23:09:42 +1000

    AOT stuff and update for lein 2.7.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject cube444Freetronics "1.0.0-SNAPSHOT"
+(defproject cube444Freetronics "1.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   
   :main cube444Freetronics.core
-  
+  :aot :all  
   )
